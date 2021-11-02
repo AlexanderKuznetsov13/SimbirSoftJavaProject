14f64f5c7eaec1befe5568eea9f6e71757e1bb24
Alexander Kuznetsov <agk13145@icloud.com>
2021-11-02 20:14:12 +0300

    [NF] добавление spring boot

---
+++ src/main/java/ru/alex/kuznetsov/project/simbirsoft/SimbirSoftApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.kuznetsov.project.simbirsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alex/kuznetsov/project/simbirsoft/SimbirSoftApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.alex.kuznetsov.project.simbirsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoftApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
