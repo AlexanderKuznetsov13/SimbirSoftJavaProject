bff339a8a3ef535e41c2dbbc46f6b73cc55c0556
Alexander Kuznetsov <agk13145@icloud.com>
2021-11-09 22:18:13 +0300

    [NF]  add mapper for project table

--- src/main/java/ru/alex/kuznetsov/project/simbirsoft/util/CommonMapper.java
+++ src/main/java/ru/alex/kuznetsov/project/simbirsoft/util/CommonMapper.java
@@ -1,7 +1,10 @@
 package ru.alex.kuznetsov.project.simbirsoft.util;
 
+import ru.alex.kuznetsov.project.simbirsoft.dto.ProjectRequestDto;
+import ru.alex.kuznetsov.project.simbirsoft.dto.ProjectResponseDto;
 import ru.alex.kuznetsov.project.simbirsoft.dto.ReleaseRequestDto;
 import ru.alex.kuznetsov.project.simbirsoft.dto.ReleaseResponseDto;
+import ru.alex.kuznetsov.project.simbirsoft.entity.ProjectEntity;
 import ru.alex.kuznetsov.project.simbirsoft.entity.ReleaseEntity;
 
 public class CommonMapper {
@@ -22,6 +25,32 @@ public class CommonMapper {
         responseDto.setProjectId(release.getProjectRelease().getId());
         return responseDto;
     }
+
+    public static ProjectEntity fromProjectRequestDtoToProjectEntity(ProjectRequestDto requestDto) {
+        ProjectEntity project = new ProjectEntity();
+        project.setId(requestDto.getId());
+        project.setName(requestDto.getName());
+        project.setDescription(requestDto.getDescription());
+        return project;
+    }
+
+    public ProjectResponseDto fromProjectEntityToProjectResponseDto(ProjectEntity project) {
+        ProjectResponseDto responseDto = new ProjectResponseDto();
+        responseDto.setId(project.getId());
+        responseDto.setName(project.getName());
+        responseDto.setDescription(project.getDescription());
+        return responseDto;
+    }
+
+//    public static ProjectEntity fromProjectRequestDtoToProjectEntity(ProjectRequestDto requestDto) {
+//        ProjectEntity project = new ProjectEntity();
+//        return project;
+//    }
+//
+//    public ProjectResponseDto fromProjectEntityToProjectResponseDto(ProjectEntity release) {
+//        ProjectResponseDto responseDto = new ProjectResponseDto();
+//        return responseDto;
+//    }
 
 
 }
