4ef28cffaf5a05f97d05c8175fab0a9f29ee34ce
Alexander Kuznetsov <agk13145@icloud.com>
2021-11-09 23:13:14 +0300

    [NF]  add service for release

---
+++ src/main/java/ru/alex/kuznetsov/project/simbirsoft/service/impl/ReleaseServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.alex.kuznetsov.project.simbirsoft.service.impl;
+
+import ru.alex.kuznetsov.project.simbirsoft.dto.ReleaseRequestDto;
+import ru.alex.kuznetsov.project.simbirsoft.dto.ReleaseResponseDto;
+import ru.alex.kuznetsov.project.simbirsoft.service.IReleaseService;
+
+public class ReleaseServiceImpl implements IReleaseService {
+    @Override
+    public ReleaseResponseDto getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ReleaseResponseDto create(ReleaseRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public ReleaseResponseDto update(ReleaseRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+}
