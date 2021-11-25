4d6bdbfd9ba32bf5f5ecbfa83607fa58f4ce77ce
Alexander Kuznetsov <agk13145@icloud.com>
2021-11-25 20:44:41 +0300

    [NF] update enum

--- src/main/java/ru/alex/kuznetsov/project/simbirsoft/enums/TaskState.java
+++ src/main/java/ru/alex/kuznetsov/project/simbirsoft/enums/TaskState.java
@@ -1,19 +1,9 @@
 package ru.alex.kuznetsov.project.simbirsoft.enums;
 
 public enum TaskState {
-    BACKLOG("BACKLOG"),
-    IN_PROGRESS("IN_PROGRESS"),
-    DONE("DONE");
+    BACKLOG,
+    IN_PROGRESS,
+    DONE;
 
-    private final String name;
-
-    TaskState(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
 
 }
--- src/main/java/ru/alex/kuznetsov/project/simbirsoft/enums/UserRole.java
+++ src/main/java/ru/alex/kuznetsov/project/simbirsoft/enums/UserRole.java
@@ -1,16 +1,9 @@
 package ru.alex.kuznetsov.project.simbirsoft.enums;
 
 public enum UserRole {
-    ADMIN("ADMIN"),
-    LEADER("LEADER"),
-    CUSTOMER("CUSTOMER"),
-    CONTRACTOR("CONTRACTOR");
-
-    private final String name;
-
-    UserRole(String name) {
-        this.name = name;
-    }
-
+    ADMIN,
+    LEADER,
+    CUSTOMER,
+    CONTRACTOR;
 }
 
